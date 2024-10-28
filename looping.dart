3b65897d8ca387212700bd372705434a0e8ac998
ATOROYO <davidatoroyosika@gmail.com>
2024-10-28 16:12:53 +0300

    Wrote Dart for…in loop

---
+++ looping.dart
@@ -0,0 +1,15 @@
+/// /  Looping Statements – Repeat Until You Get it Right / ///
+void main() {
+  // Dart for loop
+  for (int i = 1; i <= 5; i++) {
+    print("🔁 this is loop iteration $i");
+  }
+
+  // Dart for loop
+  //It only takes dart object or expression as an iterator and iterates the element one at a time.
+  var list1 = [10, 20, 30, 40, 50];
+  for (var i in list1) //for..in lop tp print list element
+  {
+    print(i); // to print the number
+  }
+}
