11800a6bac33e2d5456fb8d195d622a1126754c8
ATOROYO <davidatoroyosika@gmail.com>
2024-10-28 11:10:10 +0300

    Printed the variables

--- personalDetails.dart
+++ personalDetails.dart
@@ -3,5 +3,11 @@ void main() {
   var hobby = "Coding";
   var age = 25;
   var country = "South Sudan";
-  var dreamCar = "Chevrolet Escaled"
+  var dreamCar = "Chevrolet Escaled";
+
+  print(name);
+  print(hobby);
+  print(age);
+  print(country);
+  print(dreamCar);
 }
