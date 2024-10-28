e2b1694e5f52ce1107dd865b4278c06f36f1a4b8
ATOROYO <davidatoroyosika@gmail.com>
2024-10-28 15:39:17 +0300

    Wrote if statement

---
+++ controlFlow.dart
@@ -0,0 +1,10 @@
+// /🌟 Control Flow in Dart / ////
+void main() {
+  // 🌟 Control Flow in Dart
+  var age = 10;
+
+  if (age > 18) {
+    print("🗳️ Sarah is a voter in Uganda!");
+  }
+  print("👶🏽 Sarah is still too young to vote.");
+}
