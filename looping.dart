e7e34920a8b09162ae448c41fe2010651a2430a8
ATOROYO <davidatoroyosika@gmail.com>
2024-10-28 16:22:45 +0300

    Wrote Dart do-while loop

--- looping.dart
+++ looping.dart
@@ -12,4 +12,24 @@ void main() {
   {
     print(i); // to print the number
   }
+
+  // Dart while loop
+  var list2 = [10, 20, 30, 40, 50];
+  int i = 0; // Initialize index
+
+  while (i < list2.length) {
+    // Loop until i is less than the length of the list
+    print(list2[i]); // Print the current element at index  i
+    i++; // Increment the index
+  }
+
+  // Dart do-while loop
+  // do…while loop is similar to the while loop but only
+  // difference is that, it executes the loop statement and then check the given condition.
+  var m = 1;
+  var maxNum = 10;
+  do {
+    print("The value is: ${m}");
+    m = m + 1;
+  } while (m < maxNum);
 }
