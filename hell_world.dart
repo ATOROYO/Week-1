fd18ef6b46a0631e12a2ab24020a4638611d3e1c
ATOROYO <davidatoroyosika@gmail.com>
2024-10-28 13:31:27 +0300

    Printed more codes

--- hell_world.dart
+++ hell_world.dart
@@ -2,4 +2,14 @@
 void main() {
   print("Hello World");
   print("David Atoroyo Sika");
+
+  String wizardName = "Gandalf"; // AString spell
+  int magicLevel = 100; // Interger Magic
+  double spellPower = 75.5; // Double trouble
+  bool hasMagicStaff = true; // A magic staff? True or false
+
+  print("Name: $wizardName");
+  print("Magic Level: $magicLevel");
+  print("Spell Power: $spellPower");
+  print("Has Staff: $hasMagicStaff");
 }
